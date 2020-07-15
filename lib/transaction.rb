70746601863221bb958007bc57b7626734892d10
minsi yang <yangminsi@gmail.com>
2020-07-15 22:38:21 +0100

    transaction can be initialize with a date, test passed

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,10 +1,11 @@
 class Transaction
 
-  attr_reader :value, :balance
+  attr_reader :value, :balance, :date
 
   def initialize(value, balance)
     @value = value
     @balance = balance
+    @date = Time.now.strftime("%d/%m/%Y")
   end
 
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -2,13 +2,18 @@ require "transaction"
 
 describe Transaction do
   subject(:transaction) { Transaction.new(40, 40)}
+  let(:time) { Time.now.strftime("%d/%m/%Y") }
 
   it "should have an value when initialize" do
     expect(transaction.value).to eq 40
   end
 
-  it "should have a balance when inirialize" do
+  it "should have a balance when initialize" do
     expect(transaction.balance).to eq 40
+  end
+
+  it "should have a date when initialize" do
+    expect(transaction.date).to eq time
   end
   
 end
