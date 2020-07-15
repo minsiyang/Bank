24112cdb6acd4652dc823ea305870269465a6120
minsi yang <yangminsi@gmail.com>
2020-07-15 17:29:26 +0100

    Added #deposit, test passed

--- lib/account.rb
+++ lib/account.rb
@@ -1,10 +1,13 @@
 class Account
 
-    attr_reader :balance
+  attr_reader :balance
   
   def initialize
     @balance = 0
   end
 
+  def deposit(amount)
+    @balance += amount
+  end
 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -5,4 +5,12 @@ describe Account do
     account = Account.new
     expect(account.balance).to be 0
   end
+
+  describe "#deposit" do
+    it "increase the balance with the deposit amount" do
+      account = Account.new
+      account.deposit(100)
+      expect(account.balance).to be 100
+    end
+  end
 end
