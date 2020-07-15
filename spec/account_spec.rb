4079df7d65bc5a732614f442815c83a7edd3b815
minsi yang <yangminsi@gmail.com>
2020-07-15 17:31:59 +0100

    Added #withdraw, test passed

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,16 +1,26 @@
 require "account"
 
 describe Account do
+  subject(:account) { Account.new }
+
   it "has a default balance of 0 when initialize" do
     account = Account.new
     expect(account.balance).to be 0
   end
 
   describe "#deposit" do
-    it "increase the balance with the deposit amount" do
+    it "user can deposit money into account" do
       account = Account.new
       account.deposit(100)
       expect(account.balance).to be 100
+    end
+  end
+
+  describe "#withdraw" do
+    it "user can withdraw money from account" do
+      account.deposit(200)
+      account.withdraw(50)
+      expect(account.balance).to be 150
     end
   end
 end
